5f278c40a22bfe86389bde56133a459e9754b453
Aaron <aaron@mycujoo.tv>
2017-10-26 15:54:49 +0200

    build

--- alert/index.js
+++ alert/index.js
@@ -1,29 +1,37 @@
 const React = require('react');
-export function icon_add_alert() {
+function IconAddAlert() {
     return React.createElement(
         "svg",
         { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
         React.createElement("path", { d: "M10.01 21.01c0 1.1.89 1.99 1.99 1.99s1.99-.89 1.99-1.99h-3.98zm8.87-4.19V11c0-3.25-2.25-5.97-5.29-6.69v-.72C13.59 2.71 12.88 2 12 2s-1.59.71-1.59 1.59v.72C7.37 5.03 5.12 7.75 5.12 11v5.82L3 18.94V20h18v-1.06l-2.12-2.12zM16 13.01h-3v3h-2v-3H8V11h3V8h2v3h3v2.01z" })
     );
 }
-export function icon_error() {
+
+export { IconAddAlert, IconAddAlert as icon_add_alert };
+function IconError() {
     return React.createElement(
         "svg",
         { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
         React.createElement("path", { d: "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" })
     );
 }
-export function icon_error_outline() {
+
+export { IconError, IconError as icon_error };
+function IconErrorOutline() {
     return React.createElement(
         "svg",
         { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
         React.createElement("path", { d: "M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" })
     );
 }
-export function icon_warning() {
+
+export { IconErrorOutline, IconErrorOutline as icon_error_outline };
+function IconWarning() {
     return React.createElement(
         "svg",
         { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
         React.createElement("path", { d: "M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" })
     );
 }
+
+export { IconWarning, IconWarning as icon_warning };
