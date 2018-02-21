4ed3cf1115a65ba45d798702c29ffc40c8a6f430
Vitaliy Kibish <vitaliy@mycujoo.tv>
2018-02-21 16:40:29 +0100

    add custom mycujoo icon, add svg optimizer, update package scripts

---
+++ mycujoo/IconLineupDark/index.js
@@ -0,0 +1,109 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var React = __webpack_require__(1);
+function IconLineupDark() {
+    return React.createElement(
+        "svg",
+        { width: "23", height: "18", xmlns: "http://www.w3.org/2000/svg" },
+        React.createElement(
+            "g",
+            { fill: "none", "fill-rule": "evenodd" },
+            React.createElement("path", { d: "M-1-3h24v24H-1z" }),
+            React.createElement(
+                "g",
+                { transform: "translate(.015 .46)", fill: "#222224" },
+                React.createElement("path", { d: "M20 5.8h-2v5.4h2V5.8zM18 4h4v9h-4c-1.105 0-2-.806-2-1.8V5.8c0-.994.895-1.8 2-1.8zM2 5.8h2v5.4H2V5.8zM4 4H0v9h4c1.105 0 2-.806 2-1.8V5.8C6 4.806 5.105 4 4 4z", "fill-rule": "nonzero" }),
+                React.createElement("circle", { cx: "11.002", cy: "8.517", r: "3.5" }),
+                React.createElement("path", { d: "M1.913 1.889V15.11h18.174V1.89H1.913zM22 0v17H0V0h22z", "fill-rule": "nonzero" }),
+                React.createElement("path", { "fill-rule": "nonzero", d: "M10 0h2v16h-2z" })
+            )
+        )
+    );
+}
+
+exports.IconLineupDark = IconLineupDark;
+exports.icon_lineup_dark = IconLineupDark;
+exports.default = IconLineupDark;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ })
+/******/ ]);
