f6e3811a1b0c3ade2b66fb1002c7ea72129a1650
Aaron <aaron@mycujoo.tv>
2018-02-22 14:58:39 +0100

    fix gulp build and build custom icon

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ const gulp = require('gulp')
 const path = require('path')
 const svgmin = require('gulp-svgmin')
 
+const ICON_DIRECTORIES = 'node_modules/material-design-icons'
 const MYCUJOO_ICON_SRC = 'src/mycujoo/svg/raw'
 const MYCUJOO_ICON_DESTINATION = `${ICON_DIRECTORIES}/mycujoo/svg/production`
 
 const svgminOptions = {
     plugins: [
+        {
+            removeTitle: true
+        },
         {
             cleanupNumericValues: {
                 floatPrecision: 2,
