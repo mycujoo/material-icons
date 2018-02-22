7e1d7c44086ecbe73511d54c91f71540f437f8e0
Aaron <aaron@mycujoo.tv>
2018-02-22 14:53:15 +0100

    just use gulp for svgmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,27 @@
 const gulp = require('gulp')
-const babel = require('gulp-babel')
-const webpack = require('gulp-webpack')
-const filter = require('gulp-filter')
+const path = require('path')
+const svgmin = require('gulp-svgmin')
 
-/** Names of directories containing icons from google material icons. */
-const ICON_CATEGORIES = [
-  'action',
-  'alert',
-  'av',
-  'communication',
-  'content',
-  'device',
-  'editor',
-  'file',
-  'hardware',
-  'image',
-  'maps',
-  'navigation',
-  'notification',
-  'places',
-  'social',
-  'toggle',
-  'mycujoo', // custom icons
-]
-
-const ICON_DIRECTORIES = 'node_modules/material-design-icons'
 const MYCUJOO_ICON_SRC = 'src/mycujoo/svg/raw'
 const MYCUJOO_ICON_DESTINATION = `${ICON_DIRECTORIES}/mycujoo/svg/production`
 
-gulp.task('copy', () => {
-    return gulp.src(MYCUJOO_ICON_SRC)
-        .pipe(svgmin())
+const svgminOptions = {
+    plugins: [
+        {
+            cleanupNumericValues: {
+                floatPrecision: 2,
+            }
+        }, {
+            removeAttrs: {
+                attrs: 'fill',
+            }
+        }
+    ]
+}
+
+// parses custom icons through svgo, and moves them into the icons directory
+gulp.task('default', () => {
+    return gulp.src(path.join(MYCUJOO_ICON_SRC, '*.svg'))
+        .pipe(svgmin(svgminOptions))
         .pipe(gulp.dest(MYCUJOO_ICON_DESTINATION))
 })
-
-gulp.task('parse', () => {
-    return gulp.src(ICON_DIRECTORIES)
-        .pipe(filter(ICON_CATEGORIES))
-})
-
-gulp.task('transpile', () => {
-
-})
-
-gulp.task('default', ['copy', 'parse', 'transpile'])
