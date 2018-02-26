666197837f932461d64f7c158a1e98f20ee78e62
Vitaliy Kibish <vitaliy@mycujoo.tv>
2018-02-26 17:58:15 +0100

    remove extra block from src

--- mycujoo/IconLineup/src.js
+++ mycujoo/IconLineup/src.js
@@ -1,16 +1,7 @@
 const React = require('react');
 function IconLineup() {
     return (
-        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <g>
-                <g transform="translate(1.015 3.46)">
-                    <path d="M20 5.8h-2v5.4h2V5.8zM18 4h4v9h-4c-1.105 0-2-.806-2-1.8V5.8c0-.994.895-1.8 2-1.8zM2 5.8h2v5.4H2V5.8zM4 4H0v9h4c1.105 0 2-.806 2-1.8V5.8C6 4.806 5.105 4 4 4z"/>
-                    <circle cx="11" cy="8.52" r="3.5"/>
-                    <path d="M1.913 1.889V15.11h18.174V1.89H1.913zM22 0v17H0V0h22z"/>
-                    <path d="M10 0h2v16h-2z"/>
-                </g>
-            </g>
-        </svg>
+        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g transform="translate(1.015 3.46)"><path d="M20 5.8h-2v5.4h2V5.8zM18 4h4v9h-4c-1.105 0-2-.806-2-1.8V5.8c0-.994.895-1.8 2-1.8zM2 5.8h2v5.4H2V5.8zM4 4H0v9h4c1.105 0 2-.806 2-1.8V5.8C6 4.806 5.105 4 4 4z"/><circle cx="11" cy="8.52" r="3.5"/><path d="M1.913 1.889V15.11h18.174V1.89H1.913zM22 0v17H0V0h22z"/><path d="M10 0h2v16h-2z"/></g></svg>
     )
 }
 
