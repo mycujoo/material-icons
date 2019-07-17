312ad5bdf9675f4533d7e8d4dffabcaf9fe7cde4
Andrei Albesteanu <andrei.albesteanu@mycujoo.tv>
2019-07-17 18:33:48 +0200

    🐞 Remove dataName attribute from gender icons

--- mycujoo/IconGenderFemale/index.js
+++ mycujoo/IconGenderFemale/index.js
@@ -78,7 +78,7 @@ var React = __webpack_require__(1);
 function IconGenderFemale() {
     return React.createElement(
         "svg",
-        { dataName: "Capa 1", xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 127.7 179" },
+        { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 127.7 179" },
         React.createElement("path", { d: "M50.9 126.4V143h-10v21h10v15h25v-15h11v-21h-11v-16.4a63.84 63.84 0 1 0-25-.2zM63.6 25.2A38.75 38.75 0 1 1 24.8 64a38.84 38.84 0 0 1 38.8-38.8z" })
     );
 }
