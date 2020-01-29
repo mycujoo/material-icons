7cac048f63ac143d5b9d21dfa71b53fc8d4f5b8e
Andrei Albesteanu <andrei.albesteanu@mycujoo.tv>
2020-01-29 17:48:38 +0100

    Fix unsupported svg props on IconLikeClose

--- mycujoo/IconLikeClose/src.js
+++ mycujoo/IconLikeClose/src.js
@@ -1,7 +1,7 @@
 const React = require('react');
 function IconLikeClose() {
     return (
-        <svg width="21" height="20" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M15.225.35C18.459.35 21 2.856 21 6.045c0 3.914-3.57 7.103-8.977 11.949L10.5 19.35l-1.523-1.367C3.57 13.148 0 9.96 0 6.045 0 2.855 2.541.35 5.775.35c1.827 0 3.58.839 4.725 2.164A6.324 6.324 0 0115.225.35zM14.094 5L10.5 8.594 6.906 5 6 5.906 9.594 9.5 6 13.094l.906.906 3.594-3.594L14.094 14l.906-.906L11.406 9.5 15 5.906 14.094 5z" id="a"/></defs><use fill="#000" fillRule="nonzero" xlink:href="#a"/></svg>
+        <svg width="21" height="20" xmlns="http://www.w3.org/2000/svg"><use fill="#000" fillRule="nonzero"/></svg>
     )
 }
 
